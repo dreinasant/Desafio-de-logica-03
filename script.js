22d0a684de7468b57964386f63f124162abb5171
dreinasant <ao.andreina@gmail.com>
2022-01-20 20:49:59 -0300

    desafio-de-logica-03

---
+++ script.js
@@ -0,0 +1,13 @@
+/*for( let conta = 1; conta <=10; conta +=1){
+    console.log(conta)
+}*/
+
+/*let contador = 10
+while(1 <= contador){
+    console.log(contador)
+    contador --
+}*/
+
+for(let i = 1; i <= 100; i +=2){
+   console.log(i)
+}
